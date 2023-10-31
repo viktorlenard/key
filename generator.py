4bf4ffb1a66709cbe977460df3ea1363c0f476b0
Viktor Lenard <viktor.lenard@gmail.com>
2023-10-31 20:32:00 +0000

    Fixed bug related to amount of strings generated.

--- generator.py
+++ generator.py
@@ -2,26 +2,28 @@ import random
 import json
 
 
-f = open('/Users/viktor.lenard/Desktop/key/words.json')
+f = open('/Users/viktorlenard/Desktop/Repos/key/words.json')
 data = json.load(f)
 
 def password_generator(human, length, div):
-    password = ''
     if length < 3 or length > 6:
         raise ValueError('Invalid lenght. Must be between 3-6.')
     if div not in ['-', '.', '_']:
         raise ValueError('Invalid divider. Must be "." "_" "-" ')
     if human is True:
-        word1 = random.choice(list(data.keys()))
-        word2 = random.choice(list(data.keys()))
-        word3 = random.choice(list(data.keys()))
-        password = word1 + div + word2 + div + word3
+        words = []
+        for _ in range(length - 1):
+            words.append(random.choice(list(data.keys())))
+            words.append(div)
+        words.append(random.choice(list(data.keys())))
+        
+        password = ''.join([str(item) for item in words])
     else:
         ...
     
     return password
 
-password = password_generator(True, 3, '-')
+password = password_generator(True, 5, '.')
 print(password)
 
 ''' if __name__ == "__main__":
