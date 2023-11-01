b556ce9992afafad74f3627ffabcbb1fae7d428f
Viktor Lenard <viktor.lenard@gmail.com>
2023-11-01 11:59:09 +0000

    Implemented digits and caps for the generator

--- generator.py
+++ generator.py
@@ -1,16 +1,17 @@
 import random
 import json
+import string
 
 
 f = open('/Users/viktorlenard/Desktop/Repos/key/words.json')
 data = json.load(f)
 
 password_request = {
-    'human': True,
+    'human': False,
     'length': 3,
-    'div': '-',
-    'caps': False,
-    'nums': False,
+    'div': '_',
+    'caps': True,
+    'nums': True,
     'valid': None
 }
 
@@ -37,17 +38,32 @@ def request_validator(pr):
 def password_generator(pr):
     if pr['valid'] != True:
         raise ValueError('Invalid request. Cannot generate.')
+    words = []
     if pr['human'] is True:
-        words = []
-        for _ in range(pr['length'] - 1):
+        if pr['caps'] is True:
+            for _ in range(pr['length'] - 1):
+                words.append(random.choice(list(data.keys())).title())
+                words.append(pr['div'])
+            words.append(random.choice(list(data.keys())).upper())
+        elif pr['caps'] is False:
+            for _ in range(pr['length'] - 1):
+                words.append(random.choice(list(data.keys())))
+                words.append(pr['div'])
             words.append(random.choice(list(data.keys())))
-            words.append(pr['div'])
-        words.append(random.choice(list(data.keys())))
-        
         password = ''.join([str(item) for item in words])
+        if pr['nums'] is True:
+            numbers = ''.join(random.choice(string.digits)for i in range(2))
+            password = password + numbers
+
     else:
-        ...
-    
+        for _ in range(pr['length'] - 1):
+            word = ''.join(random.choice(string.ascii_letters + string.digits + '!#$%&*') for i in range(random.randint(5, 8)))
+            words.append(word)
+            words.append(pr['div'])
+        word = ''.join(random.choice(string.ascii_letters + string.digits + '!#$%&*') for i in range(random.randint(5, 8)))
+        words.append(word)
+        password = ''.join([str(item) for item in words])
+
     return password
 
 def main():
