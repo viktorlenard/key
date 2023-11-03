4e68574a902823213db49be085731ca7607d70c1
Viktor Lenard <viktor.lenard@gmail.com>
2023-11-03 11:54:10 +0000

    Trying to import functions form other file.

--- key/password/views.py
+++ key/password/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils import request_validator, password_generator, password_request
 
-# Create your views here.
+# Create your views here. Logic goes here.
 
 def index(request):
-    return render(request, "password/index.html")
+    request_validator(password_request)
+    password = password_generator(password_request)
+
+    return render(request, "password/index.html", {
+        "password": password
+    })
 
 def login(request):
     return render(request, "password/login.html")
 
 def profile(request):
-    return HttpResponse("User profile page")
+    return render(request, "password/profile.html")
 
 def about(request):
-    return HttpResponse("About page")
+    return render(request, "password/about.html")
